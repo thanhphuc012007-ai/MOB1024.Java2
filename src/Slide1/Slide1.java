9a989abd6c5fa9adca40bb7c366ba8f7fe0c94f1
unknown <thanhphuc01.2007@gmail.com>
2026-03-14 19:55:05 +0700

    Upload source code

---
+++ src/Slide1/Slide1.java
@@ -0,0 +1,11 @@
+package Slide1;
+
+public class Slide1 {
+	public static void main(String[] args) {
+		System.out.println("26 PS48479 Ngô Trần Thanh Phúc");
+		Xe xe = new Xe();
+		xe.setHangXe("Toyota");
+		xe.setNamSanXuat(2026);
+		System.out.println(xe.toString());	
+		}
+}
